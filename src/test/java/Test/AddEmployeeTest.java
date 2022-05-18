c1779fa12b03d64eed5e7ec0eba6776e09321e93
chief-halfoat <johnathan.j.hart@gmail.com>
2022-05-18 13:41:56 -0400

    TestNG Review HW: Add Multiple Employees from Excel File - Complete

--- src/test/java/Test/AddEmployeeTest.java
+++ src/test/java/Test/AddEmployeeTest.java
@@ -1,12 +1,18 @@
 package Test;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import utils.CommonMethods;
 import utils.ConfigReader;
+import utils.Constants;
+import utils.ExcelReader;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class AddEmployeeTest extends CommonMethods {
 //    read the configuration file for username and password
@@ -44,11 +50,46 @@ public class AddEmployeeTest extends CommonMethods {
     }
 
     @Test
-    public void addMultipleEmployee() {
+    public void addMultipleEmployee() throws InterruptedException {
 //read the employee data from excel file
 //assert that u have succesfully added the employee
 
+        login.LoginMethod(ConfigReader.getPropertyValue("username"),
+                ConfigReader.getPropertyValue("password"));
+
+        click(dash.pimOption);
+        click(dash.addEmployeeButton);
+        List<Map<String,String>> newEmployees = ExcelReader.excelIntoMap(Constants.TESTDATA_FILEPATH,"EmployeeData");
+        Iterator<Map<String,String>> itr = newEmployees.iterator();
+        while(itr.hasNext()){
+            Map<String,String> mapNewEmp = itr.next();
+            sendText(addEmployeePage.firstNameField, mapNewEmp.get("FirstName"));
+            sendText(addEmployeePage.middleNameField, mapNewEmp.get("MiddleName"));
+            sendText(addEmployeePage.lastNameField, mapNewEmp.get("LastName"));
+            String empIdValue = addEmployeePage.empIDLocator.getAttribute("value");
+
+            click(addEmployeePage.saveButton);
+
+            Thread.sleep(3000);
+            click(employeeSearchPage.empListOption);
+            sendText(employeeSearchPage.idField, empIdValue);
+            click(employeeSearchPage.searchButton);
+            SoftAssert softAssert = new SoftAssert();
+            List<WebElement> rowData = driver.findElements(By.xpath("//*[@id='resultTable']/tbody/tr"));
+            for (int i = 0; i < rowData.size(); i++) {
+                String rowText = rowData.get(i).getText();
+                System.out.println(rowText);
+                String expectedData = empIdValue+" "+mapNewEmp.get("FirstName")+" "+mapNewEmp.get("MiddleName")+" "+mapNewEmp.get("LastName");
+                Assert.assertEquals(expectedData,rowText);
+//                softAssert.assertEquals(expectedData,rowText);
+//                softAssert.assertAll();
+            }
+//            List<WebElement> rowData = addEmployeePage.rowData;
+//            for (WebElement data : rowData) {
+//                String actualID = data.getText();
+//                Assert.assertEquals(actualID, empIdValue);
+//            }
+            click(dash.addEmployeeButton);
+        }
     }
-
-
 }
--- src/test/java/pages/AddEmployeePage.java
+++ src/test/java/pages/AddEmployeePage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utils.CommonMethods;
 
+import java.util.List;
+
 public class AddEmployeePage extends CommonMethods {
 
     @FindBy(id="firstName")
@@ -36,6 +38,9 @@ public class AddEmployeePage extends CommonMethods {
 
     @FindBy(id="re_password")
     public WebElement confirmPassword;
+
+    @FindBy (xpath = "//*[@id='resultTable']/tbody/tr/td[2]")
+    public List<WebElement> rowData;
 
     public AddEmployeePage(){
         PageFactory.initElements(driver, this);
